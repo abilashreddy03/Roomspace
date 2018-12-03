7876d1008f15b6d1964b93a526f3d50c3bc70893
Dell <Dell@ABILASH>
2018-12-03 23:31:39 +0530

    roomspace

---
+++ Roomonespace/Models/Space.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomonespace.Models
+{
+    public class Space
+    {
+        
+            [Required]
+            public int Id { get; set; }
+            [Required]
+            public string Room { get; set; }
+            public string Name { get; set; }
+            [Required]
+            public string Team { get; set; }
+            [Required]
+            public string StartTime { get; set; }
+            [Required]
+            public string EndTime { get; set; }
+            [Required]
+            public string Available { get; set; }
+        }
+    
+    }
